3d2bda24af2c63f1bb160d2e27192660bcaae8a8
olafkotur <olafkotur97@gmail.com>
2019-10-21 00:53:39 +0100

    Cleaned up server code

--- server/src/Server.java
+++ server/src/Server.java
@@ -1,15 +1,60 @@
 import java.net.InetSocketAddress;
-
 import com.sun.net.httpserver.HttpServer;
+import java.util.Map;
+import java.util.HashMap;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URI;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.Headers;
+import org.json.JSONObject;
 
 public class Server {
-  public static void main(String[] args) throws Exception {
-    int port = 8080;
 
-    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-    server.createContext("/", new Handler());
+  static final int PORT = 8080;
+
+  public static void main(String[] args) throws Exception {
+    HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+    server.createContext("/api", Server::handleRequest);
     server.start();
 
-    System.out.println("Listening on Port " + port + "...");
+    System.out.println("Listening on Port " + PORT + "...");
+  }
+
+  public static void handleRequest(HttpExchange ex) throws IOException {
+    printRequest(ex);
+
+    String uri = ex.getRequestURI().toString();
+    String response = getResponse(uri);
+
+    ex.sendResponseHeaders(200, response.length());
+    OutputStream os = ex.getResponseBody();
+    os.write(response.getBytes());
+    os.close();
+  }
+
+  public static void printRequest(HttpExchange ex) {
+    Headers headers = ex.getRequestHeaders();
+    String method = ex.getRequestMethod().toString();
+    String uri = ex.getRequestURI().toString();
+
+    System.out.println("\n\n*** Headers ***");
+    headers.entrySet().forEach(System.out::println);
+    System.out.println("\nMethod: " + method);
+    System.out.println("URI: " + uri);
+  }
+
+  public static String getResponse(String uri) {
+    JSONObject res = new JSONObject();
+
+    if (uri.equals("/api/auctions")) {
+      res.put("code", 1);
+      res.put("status", "success");
+      res.put("message", "It's working! :)");
+      return res.toString();
+    }
+
+    return "";
   }
 }
