47fdcb1a825c23ae9b5d60f1f46437bfaf91b454
olafkotur <olafkotur97@gmail.com>
2019-10-21 00:56:33 +0100

    Cleaned up client code

--- client/src/Client.java
+++ client/src/Client.java
@@ -1,7 +1,11 @@
+import java.io.IOException;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class Client {
   public static void main(String[] args) throws Exception {
     System.out.println("Making requests...");
-    Requests request = new Requests();
 
     // Test URLs
     String[] testURLs = {
@@ -10,8 +14,17 @@ public class Client {
 
     // Run tests
     for (int i = 0; i < testURLs.length; i++) {
-      String res = request.run(testURLs[i]);
+      String res = sendRequest(testURLs[i]);
       System.out.println(res);
     }
   }
+
+  public static String sendRequest(String url) throws IOException {
+  OkHttpClient client = new OkHttpClient();
+  Request request = new Request.Builder().url(url).build();
+
+  try (Response response = client.newCall(request).execute()) {
+    return response.body().string();
+  }
+}
 }
